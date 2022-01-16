f231ed4517ec54ef069425539f12656816f3a678
crmrajat <crmrajat@gmail.com>
2022-01-16 16:25:57 +0530

    added the header and the footer file 🦧

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import './Header.css'
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="header-container">
+                <p>Hey there i am a common component</p>
+            </div>
+        )
+    }
+}
+
+
+export default Header;
